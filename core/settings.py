ae2f56b9d9862e26c499bcfc40dc762f7de267d9
Daniel Cintra <dany.webd3v@gmail.com>
2023-08-18 21:40:09 -0400

    feature: startapp accounts
    - start app
    - add app to settings installed apps
    - create test package

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Project apps
+    'accounts.apps.AccountsConfig',
     # Third party packages
     'rest_framework',
 ]
